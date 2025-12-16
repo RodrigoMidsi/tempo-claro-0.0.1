95cff3927ab70d8c750a8f14a06aef86bdd261c5
Rodrimir <rodrigo.miranda.silva.rms@gmail.com>
2025-12-16 15:58:05 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,8 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/routine"
-            element={
-              <ProtectedRoute>
-                <RoutinePage />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/dashboard" element={<ProtectedRoute> <DashboardPage /> </ProtectedRoute>}/>
+          <Route path="/routine" element={<ProtectedRoute> <RoutinePage /> </ProtectedRoute>}/>
           <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </AuthProvider>
